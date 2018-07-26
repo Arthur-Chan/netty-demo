9c34cb8ee4cdb4a9c5e7739accde063b0f5057c4
chenzhou <luo.chen.cl@gmail.com>
2018-07-26 21:51:04 +0800

    netty

---
+++ netty-server/src/main/java/org/chan/AppServer.java
@@ -0,0 +1,10 @@
+package org.chan;
+
+/**
+ * Hello world!
+ */
+public class AppServer {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ netty-server/src/test/java/org/chan/AppServerTest.java
@@ -0,0 +1,12 @@
+package org.chan;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppServerTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
